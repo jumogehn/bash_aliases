b456daf610b468faed082533dbeb46cb1e57f9f1
Jumogehn H <jumogehn@gmail.com>
2023-12-12 10:10:37 +0900

    .gitconfig added

--- .bash_aliases
+++ .bash_aliases
@@ -8,3 +8,12 @@ alias l='clear && ls -lbFh'
 alias ll='ls -lbFh'
 alias la='ls -a'
 alias lla='ls -al'
+
+alias gi='git'
+
+alias podst='podman stop container_latest'
+alias podrm='podman rm container_latest'
+
+alias ifocnfig='ifconfig'
+alias fiocnfig='ifconfig'
+alias ficonfig='ifconfig'
--- install.sh
+++ install.sh
@@ -1,26 +1,25 @@
-#!/bin/bash
+##!/bin/bash
+##!/bin/ksh
 
 # Must run where those files exist
-if [ -e ".bash_aliases" ]; then
-
-# Remove previously installed .bash_aliases file
-if [ -e "$HOME/.bash_aliases" ]; then
-  rm -f $HOME/.bash_aliases
-fi
+if [ -e ".bash_aliases" -a \
+  -e ".gitconfig" ]; then
 
 # Remove previously installed symbolic links
 if [ -L "$HOME/.bash_aliases" ]; then
   rm -f $HOME/.bash_aliases
 fi
+if [ -L "$HOME/.gitconfig" ]; then
+  rm -f $HOME/.gitconfig
+fi
 
-# Create new symbolic links to the .vimrc files checked-out
-ln -s -T `pwd`/.bash_aliases $HOME/.bash_aliases
+# Create new symbolic links to the *.vimrc files
+ln -s `pwd`/.bash_aliases  $HOME/.bash_aliases
+ln -s `pwd`/.gitconfig  $HOME/.gitconfig
 
-# Done!!
-echo
-echo ====================================
-echo Install .bash_aliases has been done!
-echo ====================================
+echo "=============================================="
+echo "You must make sure $HOME/.bin is in your PATH env."
+echo "=============================================="
 echo
 
 fi
